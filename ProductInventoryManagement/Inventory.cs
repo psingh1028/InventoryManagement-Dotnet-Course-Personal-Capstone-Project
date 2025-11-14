51be2198663972d8e7aaea5b0b9bcf80675c845e
Pawanjot Singh <Pawanjotsingh1997@gmail.com>
2025-11-14 18:38:01 -0500

    Initial Commit - added basic inventory class and program class containing basic add, search, update, remove and print objects

---
+++ ProductInventoryManagement/Inventory.cs
@@ -0,0 +1,58 @@
+﻿
+namespace ProductInventory
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    public class Inventory //this class will manage the list of products 
+    {
+        List<Product> products = new List<Product>(); //creating an empty list to hold product objects
+
+
+        public void AddProduct(Product product) //method to add procuts to list
+        {
+            products.Add(product);
+        }
+
+        public List<Product> GetProduct() //method to return the entier list of the products
+        {
+            return products;
+        }
+
+        public void GetProdDetails(string name) //  method to return the details of the product based on the name provided
+        {
+            foreach (Product prod in products)
+            {
+                if (prod.Name == name)
+                {
+                    Console.WriteLine($"Product Name: {prod.Name}, Price: {prod.Price}, Quantity: {prod.Quantity}");
+                }
+            }
+
+        }
+
+        public void UpdateProduct(string name, int price, int quantity) //method to update the details of the current product
+        {
+            foreach (Product prod in products)
+            {
+                if (prod.Name == name)
+                {
+                    Console.WriteLine($"is this the correct product to update? Name:{prod.Name}, Price: {prod.Price}, Quantity: {prod.Quantity} Y/N");
+                    string answer = Console.ReadLine();// add exception handling here 
+
+                    switch (answer)
+                    {
+                        case "y":
+                            prod.Price = price;
+                            prod.Quantity = quantity;
+                            Console.WriteLine("Product updated successfully.");
+                            return;
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ ProductInventoryManagement/Program.cs
@@ -0,0 +1,131 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+using System;
+using System.Collections.Generic;
+
+namespace ProductInventory
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Inventory inventory = new Inventory();
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("Please choose between the following options:\n" +
+                    "1.Add New Product \n2.Remove Product \n3.Search Product \n4.Update Item \n5.View All Products");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1": // Add New Product
+                        Console.WriteLine("Enter Product Name:");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Enter the Product Price:");
+                        int price = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the Product Quantity:");
+                        int quantity = Convert.ToInt32(Console.ReadLine());
+                        inventory.AddProduct(new Product(name, quantity, price));
+                        break;
+
+                    case "2": // Remove Product
+                        Console.WriteLine("Enter the name of the product to remove:");
+                        Console.ReadKey();
+                        break;
+
+                    case "3":// Search Product Details
+                        Console.WriteLine("Enter the name of the product to search:");
+                        inventory.GetProdDetails(Console.ReadLine());
+                        break;
+                    case "4":
+                        Console.WriteLine("Please enter the name of the product to update:");
+                        string prodName = Console.ReadLine();
+                        Console.WriteLine("Enter the new Price:");
+                        int newPrice = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the new Quantity:");
+                        int newQuantity = int.Parse(Console.ReadLine());
+                        inventory.UpdateProduct(prodName, newPrice, newQuantity);
+                        break;
+                    case "5": // View All Products
+                        List<Product> products = inventory.GetProduct();
+                        foreach (Product prod in products)
+                        {
+                            Console.WriteLine(prod.ToString());
+                        }
+                        break;
+
+                }
+
+                Console.ReadKey();
+            }
+        }
+
+    }
+
+    public class Product
+    {
+        private int _price;
+        private int _quantity;
+        private string _name;
+
+        //constructor
+
+        public Product(string name, int quantity, int price)
+        {
+            this._price = price;
+            this._quantity = quantity;
+            this._name = name;
+        }
+
+        // lets set some properties with basic validation
+        public int Price
+        {
+            get { return _price; }
+
+            set
+            {
+                if (value >= 0)
+                {
+                    _price = value;
+                }
+            }
+        }
+
+        public int Quantity
+        {
+
+            get { return _quantity; }
+
+            set
+            {
+                if (value >= 0)
+                {
+                    _quantity = value;
+                }
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+
+            set
+            {
+                if (value != null)
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - ${Price} - {Quantity} units";
+        }
+    }
+
+}
+
+
